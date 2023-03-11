9709c5c811d4ae783aa232cb121dac2be6768ece
FelipePerezTevar <felipepereztevar@gmail.com>
2023-03-11 12:43:46 +0100

    Creación de todo los componentes necesarios para la versión 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+import { ProductoDetailComponent } from './pages/producto-detail/producto-detail.component';
+import { QuienesComponent } from './pages/quienes/quienes.component';
+import { ContactoComponent } from './pages/dialogo/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductoComponent,
+    ProductoDetailComponent,
+    QuienesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
