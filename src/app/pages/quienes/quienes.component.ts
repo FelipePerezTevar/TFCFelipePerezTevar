1403e9cf4b2accc741c262ccdc666d1ea586baea
FelipePerezTevar <felipepereztevar@gmail.com>
2023-04-07 12:06:55 +0200

    Implementación del mapa
    Se ha implementado un mapa funcional en el apartado de quienes somos

--- src/app/pages/quienes/quienes.component.ts
+++ src/app/pages/quienes/quienes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {OwlOptions} from "ngx-owl-carousel-o";
+import {Map, marker, tileLayer} from "leaflet";
 
 @Component({
   selector: 'app-quienes',
@@ -34,5 +35,17 @@ export class QuienesComponent {
     src: "../../../assets/imagenes/maceta3New.png",
     alt: "Maceta3"
   },]
+
+  ngAfterViewInit(): void{
+    const map = new Map('map').setView([39.48951, -0.45171],13);
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    const markerItem = marker([39.48951, -0.45171]).addTo(map).bindPopup("Cerámicas Doli");
+    map.fitBounds([
+      [markerItem.getLatLng().lat,markerItem.getLatLng().lng]
+    ]);
+  }
 
 }
