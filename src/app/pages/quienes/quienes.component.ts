6f42af7ea21bb14e896226afc7faa3dbe6c8cc26
FelipePerezTevar <felipepereztevar@gmail.com>
2023-04-07 10:53:13 +0200

    implementación de quienes somos
    La base del apartado de quienes somos ya está instaurada

--- src/app/pages/quienes/quienes.component.ts
+++ src/app/pages/quienes/quienes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {OwlOptions} from "ngx-owl-carousel-o";
 
 @Component({
   selector: 'app-quienes',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./quienes.component.scss']
 })
 export class QuienesComponent {
+
+  options: OwlOptions = {
+    loop: true,
+    center: true,
+    mouseDrag: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  }
+
+  slidesStore = [{
+    id: 1,
+    src: "../../../assets/imagenes/maceta1New.png",
+    alt: "Maceta1"
+  },{
+    id: 2,
+    src: "../../../assets/imagenes/maceta2New.png",
+    alt: "Maceta2"
+  },{
+    id: 3,
+    src: "../../../assets/imagenes/maceta3New.png",
+    alt: "Maceta3"
+  },]
 
 }
